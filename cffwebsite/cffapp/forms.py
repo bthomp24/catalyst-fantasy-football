070839e5cdf450602e8d623d57bd5196acf8ea22
Brandon Thompson <43768961+bthomp24@users.noreply.github.com>
2023-07-17 11:23:58 -0500

    add blog categories

--- cffwebsite/cffapp/forms.py
+++ cffwebsite/cffapp/forms.py
@@ -1,15 +1,23 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+#choices = [('podcast', 'podcast'), ('rankings', 'rankings'), ('advice', 'advice'),]
+cats = Category.objects.all().values_list('name', 'name')
+
+cat_list = []
+for item in cats:
+    cat_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=cat_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
