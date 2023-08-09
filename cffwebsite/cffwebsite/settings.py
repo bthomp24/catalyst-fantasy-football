80b04527afa9715a45077936635c85f50facd3bd
Brandon Thompson <43768961+bthomp24@users.noreply.github.com>
2023-08-09 10:17:17 -0500

    Changes for production

--- cffwebsite/cffwebsite/settings.py
+++ cffwebsite/cffwebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a*c!tn1thhmzrcisn6#3txk65wuc=d#c3qofgp&*urfnew&nea'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['CatalystFF.pythonanywhere.com']
 
 
 # Application definition
