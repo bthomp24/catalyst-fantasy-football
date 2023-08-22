7abd6307680ee3bd73598569d35b4eb923b619f5
Brandon Thompson <43768961+bthomp24@users.noreply.github.com>
2023-08-22 11:30:04 -0500

    import os

--- cffwebsite/cffwebsite/settings.py
+++ cffwebsite/cffwebsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/CatalystFF/catalyst-fantasy-football/cffwebsite/cffapp/static'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # MEDIA_ROOT = BASE_DIR/'media'
 # MEDIA_URL = '/media/'
