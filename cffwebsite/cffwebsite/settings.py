ec68ad4647d06e080ac5927aab66b39a11359d85
Brandon Thompson <43768961+bthomp24@users.noreply.github.com>
2023-08-22 11:21:33 -0500

    update

--- cffwebsite/cffwebsite/settings.py
+++ cffwebsite/cffwebsite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '/home/CatalystFF/catalyst-fantasy-football/cffwebsite/cffapp/static'
 
 # MEDIA_ROOT = BASE_DIR/'media'
 # MEDIA_URL = '/media/'
